eb940a11881dddaf16df9eba7c422c17a4821f02
murilommen <murilommen@gmail.com>
2023-07-17 17:25:15 -0300

    test

--- tests/test_something.py
+++ tests/test_something.py
@@ -1,6 +1,10 @@
 import os
+import logging
+
+logger = logging.getLogger(__name__)
 
 ORG_ID = os.environ["ORG_ID"]
 
 def test_dummy() -> None:
+    logger.info(ORG_ID)
     assert ORG_ID == "org-fjx9Rz"
